c2529c49c9448ef95bb7615afd1b3bf014e50451
allen <allenlikeu@gmail.com>
2024-11-04 13:12:03 +0800

    feat: 完成docker镜像构建

--- Leek/application/app-etl/src/main/java/club/cybecraftman/leek/application/EtlApplication.java
+++ Leek/application/app-etl/src/main/java/club/cybecraftman/leek/application/EtlApplication.java
@@ -1,7 +1,15 @@
 package club.cybecraftman.leek.application;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
 public class EtlApplication {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(EtlApplication.class, args);
     }
+
 }
