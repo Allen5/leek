db622fc78ca4731a393ed13fa815465b71b1d83a
allen <allenlikeu@gmail.com>
2024-11-05 17:24:08 +0800

    feat: 新增meta库

--- Leek/infrastructure/database/src/main/java/club/cybecraftman/leek/infrastructure/database/AdminDataSourceConfig.java
+++ Leek/infrastructure/database/src/main/java/club/cybecraftman/leek/infrastructure/database/AdminDataSourceConfig.java
@@ -1,12 +1,10 @@
 package club.cybecraftman.leek.infrastructure.database;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
