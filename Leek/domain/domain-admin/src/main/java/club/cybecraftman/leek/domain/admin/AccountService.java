768b7ddf8665818e03fe2a0521b1ec8eca4be999
allen <allenlikeu@gmail.com>
2024-11-12 14:14:47 +0800

    feat: 完成token持久化

--- Leek/domain/domain-admin/src/main/java/club/cybecraftman/leek/domain/admin/AccountService.java
+++ Leek/domain/domain-admin/src/main/java/club/cybecraftman/leek/domain/admin/AccountService.java
@@ -2,13 +2,16 @@ package club.cybecraftman.leek.domain.admin;
 
 import club.cybecraftman.leek.common.constant.admin.UserStatus;
 import club.cybecraftman.leek.common.exception.LeekException;
-import club.cybecraftman.leek.domain.admin.vo.Token;
+import club.cybecraftman.leek.repo.admin.model.Token;
 import club.cybecraftman.leek.repo.admin.model.User;
+import club.cybecraftman.leek.repo.admin.repository.ITokenRepo;
 import club.cybecraftman.leek.repo.admin.repository.IUserRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -21,12 +24,16 @@ public class AccountService {
     @Autowired
     private IUserRepo userRepo;
 
+    @Autowired
+    private ITokenRepo tokenRepo;
+
     /**
      * 登录
      * @param username
      * @param password
      * @return
      */
+    @Transactional
     public Token login(final String username, final String password) throws LeekException {
         Optional<User> op = userRepo.findOneByUsernameAndStatus(username, UserStatus.NORMAL.getStatus());
         if ( op.isEmpty() ) {
@@ -37,11 +44,17 @@ public class AccountService {
         if ( !user.getPassword().equalsIgnoreCase(password) ) {
             throw new LeekException("用户名或密码错误");
         }
-        // TODO: 存入数据库
-        return Token.builder()
-                .token(tokenService.generate(user))
-                .refreshToken(tokenService.generateRefreshToken(user))
-                .build();
+        // 先删除
+        tokenRepo.deleteByUserId(user.getId());
+        // 再新建
+        Token token = new Token();
+        token.setUserId(user.getId());
+        token.setToken(tokenService.generate(user));
+        token.setRefreshToken(tokenService.generateRefreshToken(user));
+        token.setCreatedAt(new Date());
+        token.setUpdatedAt(new Date());
+        tokenRepo.save(token);
+        return token;
     }
 
 }
