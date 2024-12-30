5114b598cb9fdc4d0f6a7fd056d3084becffa7e4
Allen.Wu <allenlikeu@gmail.com>
2024-12-30 17:48:20 +0800

    feat: 修正Bug

--- Leek/repository/repo-monitor/src/main/java/club/cybercraftman/leek/repo/monitor/model/AdminActionLog.java
+++ Leek/repository/repo-monitor/src/main/java/club/cybercraftman/leek/repo/monitor/model/AdminActionLog.java
@@ -1,16 +1,19 @@
 package club.cybercraftman.leek.repo.monitor.model;
 
-import lombok.Data;
-import lombok.ToString;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * 操作记录
  */
 @Entity(name = "admin_action_log")
-@Data
+@Getter
+@Setter
+@RequiredArgsConstructor
 @ToString
 public class AdminActionLog {
 
@@ -32,4 +35,19 @@ public class AdminActionLog {
     @Column(name = "updated_at", nullable = false)
     private Date updatedAt;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        AdminActionLog that = (AdminActionLog) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
